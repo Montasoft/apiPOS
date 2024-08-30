84314258f8d0998fe92e22d3b6572957fa876ce4
JorgeDell <jorge.montagut@GMAIL.COM>
2024-08-30 07:10:17 -0500

    ajustado para CORS

--- apipos/settings.py
+++ apipos/settings.py
@@ -42,9 +42,11 @@ INSTALLED_APPS = [
     'apps.compras',
     'apps.inventario',  
     'apps.ventas',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -71,6 +73,12 @@ TEMPLATES = [
         },
     },
 ]
+
+# Permitir el dominio localhost:4200
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
+]
+
 
 WSGI_APPLICATION = 'apipos.wsgi.application'
 
